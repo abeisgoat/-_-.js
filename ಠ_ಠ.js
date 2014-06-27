c324aca9da61e7355f28ca307c8da48f96ad02f1
Shaper <shaper.pmp@gmail.com>
2014-06-27 10:18:45 +0100

    If the look of disapproval maps to anything then surely it should be a *warning*

--- ಠ_ಠ.js
+++ ಠ_ಠ.js
@@ -1,4 +1,4 @@
 (function () {
   "use strict";
-  window['ಠ_ಠ'] = function () {typeof console !== "undefined"? console.log.apply(console, arguments):null};
+  window['ಠ_ಠ'] = function () {typeof console !== "undefined"? console.warn.apply(console, arguments):null};
 }());
