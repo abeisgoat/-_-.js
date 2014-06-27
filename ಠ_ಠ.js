030bf94a5d4dc080701cdde43e69d1bd3909520d
Calvin Metcalf <calvin.metcalf@gmail.com>
2014-06-27 05:45:43 -0400

    compatibility
    should work in node.js, web workers (which do have console.log) and old shitty versions of IE where console.log isn't a real function.

--- ಠ_ಠ.js
+++ ಠ_ಠ.js
@@ -1,4 +1,8 @@
-(function () {
+(function (self) {
   "use strict";
-  window['ಠ_ಠ'] = function () {typeof console !== "undefined"? console.log.apply(console, arguments):null};
-}());
+  if ((typeof console !== 'undefined') && (typeof console.log === 'function')) {
+    self['ಠ_ಠ'] = Function.prototype.bind.call(console.log, console);
+  } else {
+    self['ಠ_ಠ'] = function () {}
+  }
+}(typeof window !== 'undifined'? window : typeof global !== 'undefined' ? global : self));
